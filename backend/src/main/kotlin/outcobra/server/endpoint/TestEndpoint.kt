11debb16041ddeb56a27098ce4f2a86a7d2e5e60
Joel Messerli <joel@jmnw.me>
2016-10-17 21:56:17 +0200

    Querydsl works on my machine

--- backend/src/main/kotlin/outcobra/server/endpoint/TestEndpoint.kt
+++ backend/src/main/kotlin/outcobra/server/endpoint/TestEndpoint.kt
@@ -2,16 +2,15 @@ package outcobra.server.endpoint
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import outcobra.server.model.ExamRepository
+import javax.inject.Inject
 
 @RestController
 @RequestMapping("/test")
-class TestEndpoint {
+class TestEndpoint @Inject constructor(val examRepository: ExamRepository) {
     @RequestMapping("/unsafe")
     fun unsafe(): String = "Unsafe hello"
 
     @RequestMapping("/safe")
     fun safe(): String = "Safe hello"
-
-    fun test() {
-    }
 }
