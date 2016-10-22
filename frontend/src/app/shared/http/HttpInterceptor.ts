123b2ad9895552ba7da437e594b929ced8d9beb7
M4R1KU <mario99.kunz@gmail.com>
2016-10-22 17:15:46 +0200

    added HttpInterceptor to shared modules added underscore library

---
+++ frontend/src/app/shared/http/HttpInterceptor.ts
@@ -0,0 +1,134 @@
+import {Injectable} from "@angular/core";
+import {
+    Http, ConnectionBackend, Request, RequestOptions, Response,
+    RequestMethod, URLSearchParams
+} from "@angular/http";
+import {Observable} from "rxjs";
+import {NotificationsService} from "angular2-notifications";
+import "rxjs/add/operator/map";
+import * as _ from "underscore";
+
+@Injectable()
+export class HttpInterceptor extends Http {
+
+    constructor(backend: ConnectionBackend, defaultOptions: RequestOptions, private _notificationsService: NotificationsService) {
+        super(backend, defaultOptions);
+    }
+
+    request(request: any): Observable<Response> {
+        request.method = (request.method || RequestMethod.Get);
+        request.url = (request.url || "");
+        request.headers = (request.headers || {});
+        request.data = (request.data || {});
+        request.params = (request.params || {});
+
+        this.interpolateUrl(request);
+
+        this.addContentType(request);
+
+        return super.request(
+            new Request({
+                method: request.method,
+                url: request.url,
+                headers: request.headers,
+                search: this.buildUrlSearchParams(request.params),
+                body: JSON.stringify(request.data)
+
+            })
+        ).catch(error => {
+            this._notificationsService.error('Error!', 'Requested resource could not be found.');
+            return Observable.empty();
+        }).map(this.unwrapHttpResponse);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////// Http Functions //////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * performs a simple get request
+     *
+     * @param url
+     * @param params
+     * @returns {Observable<Response>}
+     */
+    get(url: string, params?: any): Observable<Response> {
+        return this.request({
+            method: RequestMethod.Get,
+            url: url,
+            params: params
+        });
+    }
+
+    post(url: string, data: any, params?: any): Observable<Response> {
+        return this.request({
+            method: RequestMethod.Post,
+            url: url,
+            data: data,
+            params: params
+        })
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    ///////// Helper Functions ////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////
+
+    private interpolateUrl(requestOptions: any) {
+        requestOptions.url = requestOptions.url.replace(
+            /:([a-zA-Z]+[\w-]*)/g,
+            ($0, token) => {
+                if (_.has(requestOptions.params, token)) return this.extractValue(requestOptions.params, token);
+                if (_.has(requestOptions.data, token)) return this.extractValue(requestOptions.data, token);
+                return "";
+            }
+        );
+        // cleanup double slashes
+        requestOptions.url = requestOptions.url.replace(/\/{2,}/g, "/");
+        // cleanup unnecessary slashes at the end
+        requestOptions.url = requestOptions.url.replace(/\/+$/g, "");
+        return ( requestOptions );
+    }
+
+    private extractValue(obj, key) {
+        let value = obj[key];
+        delete obj[key];
+        return value;
+    }
+
+    /*request(url: string | Request, options?: RequestOptionsArgs): Observable<Response> {
+     return super.request(url, options)
+     .catch((err: any, caught: Observable<Response>) => {
+     this._notificationsService.error('Error!', 'Requested resource could not be found.');
+     return Observable.empty();
+     });
+     }
+
+     get(url: string, options?: RequestOptionsArgs): Observable<Response> {
+     return super.get(url, options)
+     .catch((err: any, caught: Observable<Response>) => {
+     this._notificationsService.error('Error!', 'Requested resource could not be found.');
+     return Observable.empty();
+     });
+     }*/
+
+
+    private addContentType(request: any) {
+        if (request.method !== RequestMethod.Get) {
+            request.options['Content-Type'] = "application/json; charset=UTF-8";
+        }
+        return request;
+    }
+
+    private buildUrlSearchParams(params: {}|any) {
+        let urlParams = new URLSearchParams();
+        for (let key in params) {
+            urlParams.append(key, params[key]);
+        }
+        return urlParams;
+    }
+
+    private unwrapHttpResponse(response) {
+        return response.json();
+    }
+}
+
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,9 +1,18 @@
 import {NgModule} from '@angular/core';
+import {NotificationsService} from "angular2-notifications";
+import {RequestOptions, XHRBackend} from "@angular/http";
+import {HttpInterceptor} from "./http/HttpInterceptor";
 
 @NgModule({
     declarations: [],
     exports: [],
     imports: [],
-    providers: [],
+    providers: [
+        {
+            provide: HttpInterceptor,
+            useFactory: (backend: XHRBackend, defaultOptions: RequestOptions, notificationsService: NotificationsService) => new HttpInterceptor(backend, defaultOptions, notificationsService),
+            deps: [XHRBackend, RequestOptions, NotificationsService]
+        }
+    ],
 })
 export class SharedModule{}
