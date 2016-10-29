26ab39e210f7aaece2905645c469f3796ff34252
needToRoll <fb.buergi@gmx.ch>
2016-10-30 01:51:16 +0200

    added all constructors getters setters and equals #27
    * added all constructors getters setters and equals
    
    * removed import and lombok dependecy
    
    * changes according to the review of #29

--- backend/src/main/java/outcobra/server/model/ExamTask.java
+++ backend/src/main/java/outcobra/server/model/ExamTask.java
@@ -3,14 +3,10 @@ package outcobra.server.model;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
-import lombok.*;
-
-@Data
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
 public class ExamTask {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotNull
@@ -22,4 +18,79 @@ public class ExamTask {
 
     @NotNull
     private boolean finished;
+
+    //region constructors
+
+    public ExamTask(Long id, String task, Exam exam, boolean finished) {
+        this.id = id;
+        this.task = task;
+        this.exam = exam;
+        this.finished = finished;
+    }
+
+    public ExamTask() {
+    }
+
+    //endregion
+
+    //region default functions
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public Exam getExam() {
+        return exam;
+    }
+
+    public void setExam(Exam exam) {
+        this.exam = exam;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ExamTask)) return false;
+
+        ExamTask examTask = (ExamTask) o;
+
+        if (isFinished() != examTask.isFinished()) return false;
+        if (!getId().equals(examTask.getId())) return false;
+        if (getTask() != null ? !getTask().equals(examTask.getTask()) : examTask.getTask() != null)
+            return false;
+        return getExam().equals(examTask.getExam());
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId().hashCode();
+        result = 31 * result + (getTask() != null ? getTask().hashCode() : 0);
+        result = 31 * result + getExam().hashCode();
+        result = 31 * result + (isFinished() ? 1 : 0);
+        return result;
+    }
+
+    //endregion
 }
+
