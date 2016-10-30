ab442bcc76f53f3e29d9738a30af124379422b3d
Joel Messerli <joel@jmnw.me>
2016-10-30 21:11:00 +0100

    Added login endpoint

--- backend/src/main/kotlin/outcobra/server/service/internal/DefaultUserService.kt
+++ backend/src/main/kotlin/outcobra/server/service/internal/DefaultUserService.kt
@@ -1,10 +1,8 @@
 package outcobra.server.service.internal
 
 import com.auth0.spring.security.api.Auth0UserDetails
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
-import outcobra.server.config.Auth0Client
 import outcobra.server.model.User
 import outcobra.server.model.dto.UserDto
 import outcobra.server.model.mapper.Mapper
@@ -18,14 +16,6 @@ open class DefaultUserService
 @Inject constructor(val userRepository: UserRepository,
                     val userDtoMapper: Mapper<User, UserDto>) : UserService {
 
-    @Autowired
-    private lateinit var auth0Client: Auth0Client
-
-    override fun getUserDetails(): Auth0UserDetails {
-        val auth = SecurityContextHolder.getContext().authentication
-        return auth.principal as Auth0UserDetails
-    }
-
     override fun getCurrentUser(): UserDto {
         val userDetails = getUserDetails()
         try {
@@ -34,5 +24,18 @@ open class DefaultUserService
         } catch (e: EntityNotFoundException) {
             return UserDto("", "")
         }
+    }
+
+    override fun getUserDetails(): Auth0UserDetails {
+        val auth = SecurityContextHolder.getContext().authentication
+        return auth.principal as Auth0UserDetails
+    }
+
+    override fun loginRegister() {
+        if (getCurrentUser().userId.isNotEmpty()) return
+
+        val userDetails = getUserDetails()
+        val newUser = User(userDetails.getAuth0Attribute("user_id") as String, userDetails.username, null)
+        userRepository.save(newUser)
     }
 }
