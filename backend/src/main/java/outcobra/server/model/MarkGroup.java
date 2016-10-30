76a98e987d5afe138a1a6c79c5b515bc5b356139
Florian Bürgi <fb.buergi@gmx.ch>
2016-10-30 02:03:59 +0200

    resolved merge conflicts in models after #29

--- backend/src/main/java/outcobra/server/model/MarkGroup.java
+++ backend/src/main/java/outcobra/server/model/MarkGroup.java
@@ -1,31 +1,50 @@
 package outcobra.server.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.*;
 
-import lombok.*;
-
-@Data
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 public class MarkGroup extends Mark {
+
     @OneToMany(mappedBy = "markGroup")
     private List<Mark> marks;
 
     @OneToOne(mappedBy = "markGroup")
     private Subject subject;
 
+    public MarkGroup(Double weight, Exam exam, MarkGroup markGroup, List<Mark> marks, Subject subject) {
+        super(weight, exam, markGroup);
+        this.marks = marks;
+        this.subject = subject;
+    }
+
+    public MarkGroup(List<Mark> marks, Subject subject) {
+        this.marks = marks;
+        this.subject = subject;
+    }
+
+    public MarkGroup() {
+        super();
+        this.marks = new ArrayList<>();
+    }
+
     // Todo persist
     public void addMark(Mark mark) {
         marks.add(mark);
     }
 
+    //region constructors
+
     // Todo persist
     public void removeMark(Mark mark) {
         marks.remove(mark);
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
     }
 
     @Override
@@ -40,8 +59,47 @@ public class MarkGroup extends Mark {
         return valueSum / weightSum;
     }
 
-    @Override
-    public double getWeight() {
-        return weight;
+    //endregion
+
+    //region default functions
+
+    public List<Mark> getMarks() {
+        return marks;
     }
+
+    public void setMarks(List<Mark> marks) {
+        this.marks = marks;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MarkGroup)) return false;
+        if (!super.equals(o)) return false;
+
+        MarkGroup markGroup = (MarkGroup) o;
+
+        if (getMarks() != null ? !getMarks().equals(markGroup.getMarks()) : markGroup.getMarks() != null)
+            return false;
+        return getSubject() != null ? getSubject().equals(markGroup.getSubject()) : markGroup.getSubject() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (getMarks() != null ? getMarks().hashCode() : 0);
+        result = 31 * result + (getSubject() != null ? getSubject().hashCode() : 0);
+        return result;
+    }
+
+    //endregion
 }
