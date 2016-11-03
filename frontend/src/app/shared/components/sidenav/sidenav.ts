41c97efbbe318b8fc7882e8b35260ae3a2bf861c
M4R1KU <mario99.kunz@gmail.com>
2016-11-03 20:58:50 +0100

    added open and close to sidenav

--- frontend/src/app/shared/components/sidenav/sidenav.ts
+++ frontend/src/app/shared/components/sidenav/sidenav.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
+import {
+    Component, OnInit, Input, ViewEncapsulation, ElementRef, Renderer, trigger, state, style,
+    transition, animate
+} from '@angular/core';
 
 @Component({
     selector: 'sidenav-layout',
@@ -13,17 +16,50 @@ export class SidenavLayout {
 @Component({
     selector: 'sidenav',
     template: `<ng-content></ng-content>`,
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    host: {
+        '(document: click)': 'onDocumentClick($event)'
+    }
 })
 export class SidenavComponent implements OnInit {
 
-    @Input() private width: number;
+    private sidenavOpened = false;
 
-    constructor() {
+    constructor(private elRef: ElementRef, private renderer: Renderer) {
     }
 
     ngOnInit() {
-        this.width = (this.width || 300);
+        this.updateSidenavStyle();
+    }
+
+    onDocumentClick(event: Event) {
+        if (event.target !== this.elRef.nativeElement && this.sidenavOpened) {
+            this.close();
+        }
+    }
+
+    open() {
+        this.toggle(true);
+    }
+
+    close() {
+        this.toggle(false);
+    }
+
+    toggle(isOpen: boolean) {
+        this.sidenavOpened = isOpen;
+        this.updateSidenavStyle();
+    }
+
+    updateSidenavStyle() {
+        this.renderer.setElementStyle(this.elRef.nativeElement, 'left', ((this.sidenavOpened) ? '0' : '-' + (this.width - 50)) + 'px');
+    }
+
+    get width() {
+        if (this.elRef.nativeElement) {
+            return this.elRef.nativeElement.offsetWidth;
+        }
+        return 0;
     }
 
 }
