c4e2da8fea73d89a8dff2da1b9aed5329e8ea012
Florian Bürgi <fb.buergi@gmx.ch>
2016-11-06 21:50:40 +0100

    added Uri normalization and Class Repo

--- backend/src/main/kotlin/outcobra/server/config/AppConfig.kt
+++ backend/src/main/kotlin/outcobra/server/config/AppConfig.kt
@@ -4,19 +4,20 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import outcobra.server.filter.RequestAuthorizationFilter
+import javax.inject.Inject
 
 /**
  * Created by bbuerf on 01.11.2016.
  */
 @Configuration
-class AppConfig {
-    @Bean
-    fun ApiRequestFilterRegistration(): FilterRegistrationBean {
+open class AppConfig {
+    @Bean @Inject
+    open fun ApiRequestFilterRegistration(requestAuthorizationFilter: RequestAuthorizationFilter): FilterRegistrationBean {
         var registration = FilterRegistrationBean()
         registration.addUrlPatterns("/api/*")
         registration.setName("RequestAuthorization")
         registration.order = 2
-        registration.filter = RequestAuthorizationFilter()
+        registration.filter = requestAuthorizationFilter
         return registration
     }
 }
--- backend/src/main/kotlin/outcobra/server/filter/RequestAuthorizationFilter.kt
+++ backend/src/main/kotlin/outcobra/server/filter/RequestAuthorizationFilter.kt
@@ -3,6 +3,7 @@ package outcobra.server.filter
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.filter.GenericFilterBean
@@ -11,8 +12,12 @@ import outcobra.server.model.dto.SchoolClassDto
 import outcobra.server.model.dto.TeacherDto
 import outcobra.server.model.mapper.MappableDto
 import outcobra.server.model.marker.OwnerVerifiable
+import outcobra.server.model.repository.InstitutionRepository
+import outcobra.server.model.repository.SchoolClassRepository
 import outcobra.server.service.UserService
 import java.util.*
+import java.util.regex.Pattern
+import javax.inject.Inject
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
@@ -23,10 +28,10 @@ import javax.servlet.http.HttpServletRequest
  * Created by bbuerf on 01.11.2016.
  */
 @Component
-open class RequestAuthorizationFilter : GenericFilterBean() {
+open class RequestAuthorizationFilter @Inject constructor(institutionRepository: InstitutionRepository, schoolClassRepository: SchoolClassRepository) : GenericFilterBean() {
     private val LOGGER = LoggerFactory.getLogger(javaClass)
     var endpointObjectMapping = HashMap<String, Class<out MappableDto<*, out OwnerVerifiable>>>()
-
+    var endpointRepositoryMapping = HashMap<String, JpaRepository<out OwnerVerifiable, Long>>()
     @Autowired
     lateinit var userService: UserService
 
@@ -35,13 +40,24 @@ open class RequestAuthorizationFilter : GenericFilterBean() {
         endpointObjectMapping.put("api/institution", InstitutionDto::class.java)
         endpointObjectMapping.put("api/class", SchoolClassDto::class.java)
         endpointObjectMapping.put("api/teacher", TeacherDto::class.java)
+
+        endpointRepositoryMapping.put("api/institution", institutionRepository)
+        endpointRepositoryMapping.put("api/class", schoolClassRepository)
+    }
+
+    private val uriPattern = Pattern.compile("^/?(.*?)/?$")
+
+    private fun normalizeUrl(url: String): String {
+        val matcher = uriPattern.matcher(url)
+        if (!matcher.matches()) return ""
+        else return matcher.group(1)
     }
 
     override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
         if (request is HttpServletRequest) {
             var userId = userService.getCurrentUser().userId
             var method = request.method
-            var type = endpointObjectMapping.get(request.requestURI)
+            var type = endpointObjectMapping[normalizeUrl(request.requestURI)]
             if (method.equals(RequestMethod.POST)) {
                 var json = request.reader.readText()
                 var entity = ObjectMapper().readValue(json, type).toEntity()
@@ -52,12 +68,9 @@ open class RequestAuthorizationFilter : GenericFilterBean() {
                     chain?.doFilter(request, response)
                 }
             } else if (method.equals(RequestMethod.GET)) {
-                request.parameterNames.toList().filter { it.contains("id") }.forEach {
-                    request.getParameter(it)
 
-                }
             } else if (method.equals(RequestMethod.DELETE)) {
-                request.parameterNames.toList().filter { it.contains("id") }.forEach { }
+
             }
 
 
