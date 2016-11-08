2e09f788909b5cdcb33cf4537f8a4508001985b7
Joel Messerli <joel@jmnw.me>
2016-11-08 11:24:32 +0100

    Updated auth0 version
    Fixed some runtime exceptions in userSErvice

--- backend/src/main/kotlin/outcobra/server/config/AuthConfig.kt
+++ backend/src/main/kotlin/outcobra/server/config/AuthConfig.kt
@@ -2,6 +2,7 @@ package outcobra.server.config
 
 import com.auth0.Auth0
 import com.auth0.authentication.AuthenticationAPIClient
+import com.auth0.authentication.result.UserProfile
 import com.auth0.spring.security.api.Auth0JWTToken
 import com.auth0.spring.security.api.Auth0SecurityConfig
 import org.springframework.boot.autoconfigure.security.SecurityProperties
@@ -22,10 +23,16 @@ open class AuthConfig : Auth0SecurityConfig() {
 
     override fun authorizeRequests(http: HttpSecurity?) {
         http!!.authorizeRequests()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/webjars/springfox-swagger-ui/**").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                .antMatchers("/v2/api-docs").permitAll()
                 .anyRequest().authenticated()
     }
 }
 
 class Auth0Client(val clientId: String, val domain: String, val auth0: Auth0 = Auth0(clientId, domain), val client: AuthenticationAPIClient = auth0.newAuthenticationAPIClient()) {
     fun getUsername(token: Auth0JWTToken): String = client.tokenInfo(token.jwt).execute().email
+
+    fun getUserProfile(token: Auth0JWTToken): UserProfile = client.tokenInfo(token.jwt).execute()
 }
