9f08779dd5af429ded26f8d73be1ebb28ee2b171
M4R1KU <mario99.kunz@gmail.com>
2016-11-14 20:56:51 +0100

    add more entities to the mock manage service added entity menu on the manage ui

--- backend/src/main/kotlin/outcobra/server/service/internal/DefaultManageService.kt
+++ backend/src/main/kotlin/outcobra/server/service/internal/DefaultManageService.kt
@@ -15,7 +15,7 @@ open class DefaultManageService : ManageService {
                                 1, "IET-gibb",
                                 Arrays.asList(
                                         SchoolClassDto(
-                                                1, "INF2014.55G",
+                                                1, "INF2014.5G",
                                                 Arrays.asList(
                                                         SchoolYearDto(
                                                                 1, "2016/17",
@@ -29,6 +29,77 @@ open class DefaultManageService : ManageService {
                                                                                 Arrays.asList(
                                                                                         SubjectDto(
                                                                                                 1, "Mathematik"
+                                                                                        )
+                                                                                )
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                        ),
+                                        SchoolClassDto(
+                                                1, "INF2014.5K",
+                                                Arrays.asList(
+                                                        SchoolYearDto(
+                                                                1, "2016/17",
+                                                                LocalDate.of(2016, 8, 1),
+                                                                LocalDate.of(2017, 7, 31),
+                                                                Arrays.asList(
+                                                                        SemesterDto(
+                                                                                1, "2. Semester",
+                                                                                LocalDate.of(2016, 8, 1),
+                                                                                LocalDate.of(2017, 1, 30),
+                                                                                Arrays.asList(
+                                                                                        SubjectDto(
+                                                                                                1, "Modul 201"
+                                                                                        )
+                                                                                )
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                        )
+                                )
+                        ),
+                        InstitutionDto(
+                                1, "BMS-gibb",
+                                Arrays.asList(
+                                        SchoolClassDto(
+                                                1, "BMSi2014C",
+                                                Arrays.asList(
+                                                        SchoolYearDto(
+                                                                1, "2016/17",
+                                                                LocalDate.of(2016, 8, 1),
+                                                                LocalDate.of(2017, 7, 31),
+                                                                Arrays.asList(
+                                                                        SemesterDto(
+                                                                                1, "1. Semester",
+                                                                                LocalDate.of(2016, 8, 1),
+                                                                                LocalDate.of(2017, 1, 30),
+                                                                                Arrays.asList(
+                                                                                        SubjectDto(
+                                                                                                1, "Mathematik"
+                                                                                        )
+                                                                                )
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                        ),
+                                        SchoolClassDto(
+                                                1, "BMSi2014A",
+                                                Arrays.asList(
+                                                        SchoolYearDto(
+                                                                1, "2016/17",
+                                                                LocalDate.of(2016, 8, 1),
+                                                                LocalDate.of(2017, 7, 31),
+                                                                Arrays.asList(
+                                                                        SemesterDto(
+                                                                                1, "2. Semester",
+                                                                                LocalDate.of(2016, 8, 1),
+                                                                                LocalDate.of(2017, 1, 30),
+                                                                                Arrays.asList(
+                                                                                        SubjectDto(
+                                                                                                1, "Englisch"
                                                                                         )
                                                                                 )
                                                                         )
