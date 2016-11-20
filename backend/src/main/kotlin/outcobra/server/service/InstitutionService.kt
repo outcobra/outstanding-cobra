e3c73d27896630358ec64db83e8463cd1e2f1032
Joel Messerli <joel@jmnw.me>
2016-11-20 14:19:48 +0100

    Started working on OC-CR-5

--- backend/src/main/kotlin/outcobra/server/service/InstitutionService.kt
+++ backend/src/main/kotlin/outcobra/server/service/InstitutionService.kt
@@ -2,10 +2,13 @@ package outcobra.server.service
 
 import outcobra.server.model.dto.InstitutionDto
 
+/**
+ *
+ */
 interface InstitutionService {
     fun readAllInstitutions(): List<InstitutionDto>
     fun readInstitutionById(id: Long): InstitutionDto
     fun deleteInstitution(id: Long)
-    fun createInstitution(name: String): InstitutionDto
+    fun createInstitution(institutionDto: InstitutionDto): InstitutionDto
     fun updateInstitution(institutionDto: InstitutionDto): InstitutionDto
 }
