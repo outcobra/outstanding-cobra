46ceb3dd8973d30ed3deda0df36417357f938c39
Joel Messerli <joel@jmnw.me>
2016-12-06 10:37:58 +0100

    Added jackson jdk8 and java time modules but it's not working smh

--- backend/src/main/kotlin/outcobra/server/config/AppConfig.kt
+++ backend/src/main/kotlin/outcobra/server/config/AppConfig.kt
@@ -1,10 +1,33 @@
 package outcobra.server.config
 
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import outcobra.server.filter.RequestAuthorizationFilter
 
+@Configuration
 open class AppConfig {
-    open fun ApiRequestFilterRegistration(requestAuthorizationFilter: RequestAuthorizationFilter): FilterRegistrationBean {
+
+    @Bean
+    @Primary
+    open fun jacksonBuilder(): Jackson2ObjectMapperBuilder {
+        val builder = Jackson2ObjectMapperBuilder()
+
+        builder.modules(JavaTimeModule(),
+                Jdk8Module(),
+                ParameterNamesModule(),
+                KotlinModule())
+
+        return builder
+    }
+
+    private fun ApiRequestFilterRegistration(requestAuthorizationFilter: RequestAuthorizationFilter): FilterRegistrationBean {
         val filterRegistration = FilterRegistrationBean(requestAuthorizationFilter)
         filterRegistration.addUrlPatterns("/api/*")
         filterRegistration.setName("Request Authorization Filter")
