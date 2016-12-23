67cefedd34c63d7e52842ff389f04d5b6484953b
M4R1KU <mario99.kunz@gmail.com>
2016-12-23 11:44:19 +0100

    updated to @angular/material beta.0 fixed weird behaviour of datepicker with new material version added cool spring banner

--- frontend/src/app/task/task.module.ts
+++ frontend/src/app/task/task.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from "@angular/common";
 import {TaskComponent} from "./task.component";
 import {TaskRoutingModule} from "./task-routing.module";
 import {AuthGuard} from "../shared/services/auth/auth-guard.service";
+import {MaterialModule} from "@angular/material";
 
 @NgModule({
     imports: [
         CommonModule,
-        TaskRoutingModule
+        TaskRoutingModule,
+        MaterialModule
     ],
     declarations: [
         TaskComponent
