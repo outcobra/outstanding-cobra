bc2b66db767815de9e69b7fde54276da1606477f
Mario Kunz <mario.kunz@bit.admin.ch>
2017-01-03 17:04:25 +0100

    added basic filter options

--- frontend/src/app/task/task.component.ts
+++ frontend/src/app/task/task.component.ts
@@ -14,6 +14,10 @@ export class TaskComponent implements OnInit {
     private tasks: Task[];
     private filteredTasks: Task[];
 
+    private filter = {
+        subjectId: 0
+    };
+
     constructor(private taskService: TaskService, private formBuilder: FormBuilder) {
     }
 
@@ -23,14 +27,31 @@ export class TaskComponent implements OnInit {
         });
         this.taskService.getAll()
             .subscribe((tasks: Task[]) => this.tasks = this.filteredTasks = tasks);
+
         this.filterForm.get('searchTerm').valueChanges
             .debounceTime(300)
             .distinctUntilChanged()
             .subscribe(searchTerm => this.filteredTasks = this.search(searchTerm));
     }
 
+    /**
+     * returns all tasks which contain the term in the name, the description or the subject name
+     * @param term
+     * @returns {Task[]}
+     */
     search(term: string) {
-        return this.tasks.filter((task: Task) => task.name.toLowerCase().includes(term.toLowerCase()));
+        return this.tasks.filter((task: Task) => {
+            let lcTerm = term.toLowerCase();
+            return task.name.toLowerCase().includes(lcTerm) ||
+                task.description.toLowerCase().includes(lcTerm) ||
+                task.subject.name.toLowerCase().includes(lcTerm);
+        });
+    }
+
+    filterTasks() {
+        if (this.filter.subjectId != 0) {
+            this.filteredTasks = this.tasks.filter(task => task.subject.id == this.filter.subjectId);
+        }
     }
 
 }
