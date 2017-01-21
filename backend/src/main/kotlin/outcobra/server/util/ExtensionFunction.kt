ca4ebfe4dc6a1071b137775680faa93ab5e4f511
needToRoll <fb.buergi@gmx.ch>
2017-01-21 17:15:00 +0100

    Amend/entity date validation (#102)
    * fixed update bug in validator (extension fun)
    
    * fixed #101 validation bug & cleanup

--- backend/src/main/kotlin/outcobra/server/util/ExtensionFunction.kt
+++ backend/src/main/kotlin/outcobra/server/util/ExtensionFunction.kt
@@ -12,9 +12,10 @@ import java.time.LocalDate
 fun LocalDate.isBeforeOrEqual(b: LocalDate): Boolean = this.isBefore(b) || this.isEqual(b)
 fun LocalDate.isAfterOrEqual(b: LocalDate): Boolean = this.isAfter(b) || this.isEqual(b)
 
-infix fun SchoolYear.doesNotOverlap(schoolYear : SchoolYear) : Boolean =
-        !DateUtil.isOverlap(this.validFrom, this.validTo, schoolYear.validFrom, schoolYear.validTo)
-infix fun Semester.doesNotOverlap(semester : Semester) : Boolean =
-        !DateUtil.isOverlap(this.validFrom, this.validTo, semester.validFrom, semester.validTo)
+infix fun SchoolYear.doesNotOverlap(schoolYear: SchoolYear): Boolean =
+        this.id == schoolYear.id || !DateUtil.isOverlap(this.validFrom, this.validTo, schoolYear.validFrom, schoolYear.validTo)
+
+infix fun Semester.doesNotOverlap(semester: Semester): Boolean =
+        this.id == semester.id || !DateUtil.isOverlap(this.validFrom, this.validTo, semester.validFrom, semester.validTo)
 
 
