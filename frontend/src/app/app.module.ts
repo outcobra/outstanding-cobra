1ac2acb688b0b34783009b53e8aa0f994de69df6
M4R1KU <mario99.kunz@gmail.com>
2017-01-24 10:07:54 +0100

    added factories for appmodule to prevent angular-cli errors

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,19 @@
-import {NgModule, APP_INITIALIZER} from "@angular/core";
-import {FormsModule} from "@angular/forms";
-import {BrowserModule} from "@angular/platform-browser";
-import {HttpModule, Http} from "@angular/http";
-import {MaterialModule} from "@angular/material";
-import {TranslateModule, TranslateLoader, TranslateStaticLoader, TranslateService} from "ng2-translate";
-import "rxjs/add/operator/toPromise";
-import {AppComponent} from "./app.component";
-import {Config} from "./config/Config";
-import {SharedModule} from "./shared/shared.module";
-import {SimpleNotificationsModule} from "angular2-notifications";
-import {AppRoutingModule} from "./app-routing.module";
-import {MainModule} from "./main/main.module";
-import {ManageModule} from "./manage/manage.module";
-import {TaskModule} from "./task/task.module";
+import {NgModule, APP_INITIALIZER} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule, Http} from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import {TranslateModule, TranslateLoader, TranslateService} from 'ng2-translate';
+import 'rxjs/add/operator/toPromise';
+import {AppComponent} from './app.component';
+import {Config} from './config/Config';
+import {SharedModule} from './shared/shared.module';
+import {SimpleNotificationsModule} from 'angular2-notifications';
+import {AppRoutingModule} from './app-routing.module';
+import {MainModule} from './main/main.module';
+import {ManageModule} from './manage/manage.module';
+import {TaskModule} from './task/task.module';
+import {configLoader, translateFactory, translationLoader} from './shared/services/factories';
 
 @NgModule({
     declarations: [
@@ -30,7 +31,7 @@ import {TaskModule} from "./task/task.module";
         MaterialModule.forRoot(),
         TranslateModule.forRoot({
             provide: TranslateLoader,
-            useFactory: (http: Http) => new TranslateStaticLoader(http, '/assets/i18n', '.json'),
+            useFactory: translateFactory,
             deps: [Http]
         }),
         SimpleNotificationsModule
@@ -39,16 +40,13 @@ import {TaskModule} from "./task/task.module";
         Config,
         {
             provide: APP_INITIALIZER,
-            useFactory: (config: Config, translateService: TranslateService) => () => config.load(),
+            useFactory: configLoader,
             deps: [Config],
             multi: true
         },
         {
             provide: APP_INITIALIZER,
-            useFactory: (translateService: TranslateService) => () => {
-                translateService.setDefaultLang('en');
-                return translateService.use(translateService.getBrowserLang()).toPromise();
-            },
+            useFactory: translationLoader,
             deps: [TranslateService],
             multi: true
         }
