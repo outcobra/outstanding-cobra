0b1950f365ec7ce47fa5c6057bc7701ee194a8a8
M4R1KU <mario99.kunz@gmail.com>
2017-01-24 11:05:24 +0100

    fixed app initializer
    (cherry picked from commit 3bf65a5)

--- frontend/src/app/shared/services/factories.ts
+++ frontend/src/app/shared/services/factories.ts
@@ -3,14 +3,14 @@ import {Http} from '@angular/http';
 import {TranslateStaticLoader, TranslateService} from 'ng2-translate';
 
 export function translateFactory(http: Http) {
-  return new TranslateStaticLoader(http, '/assets/i18n', '.json');
+    return new TranslateStaticLoader(http, '/assets/i18n', '.json');
 }
 
-export function configLoader(config: Config) {
-  return () => config.load()
+export function configLoader(config: Config): () => Promise<any> {
+    return () => config.load()
 }
 
-export function translationLoader(translateService: TranslateService){
-  translateService.setDefaultLang('en');
-  return translateService.use(translateService.getBrowserLang()).toPromise();
+export function translationLoader(translateService: TranslateService): () => Promise<any> {
+    translateService.setDefaultLang('en');
+    return () => translateService.use(translateService.getBrowserLang()).toPromise();
 }
