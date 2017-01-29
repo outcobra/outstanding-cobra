be48f08baff9afa4eb302eb7988f58e8bf2043c9
Florian Bürgi <fb.buergi@gmx.ch>
2017-01-29 20:32:55 +0100

    added filter to wrap request body

--- backend/src/main/kotlin/outcobra/server/config/AppConfig.kt
+++ backend/src/main/kotlin/outcobra/server/config/AppConfig.kt
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer
 import org.springframework.core.io.ClassPathResource
 import outcobra.server.filter.RequestAuthorizationFilter
+import outcobra.server.filter.RequestWrappingFilter
 
 @Configuration
 open class AppConfig {
@@ -20,12 +21,21 @@ open class AppConfig {
         return configurer
     }
 
-    private fun ApiRequestFilterRegistration(requestAuthorizationFilter: RequestAuthorizationFilter): FilterRegistrationBean {
+    @Bean
+    open fun apiRequestFilterRegistration(requestAuthorizationFilter: RequestAuthorizationFilter): FilterRegistrationBean {
         val filterRegistration = FilterRegistrationBean(requestAuthorizationFilter)
         filterRegistration.addUrlPatterns("/api/*")
         filterRegistration.setName("Request Authorization Filter")
         filterRegistration.order = 2
+        return filterRegistration
+    }
 
+    @Bean
+    open fun apiWrappingFilterRegistration(wrappingFilter: RequestWrappingFilter): FilterRegistrationBean {
+        val filterRegistration = FilterRegistrationBean(wrappingFilter)
+        filterRegistration.addUrlPatterns("/api/*")
+        filterRegistration.setName("Request wrapping Filter")
+        filterRegistration.order = 1
         return filterRegistration
     }
 }
