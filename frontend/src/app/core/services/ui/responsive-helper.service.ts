b34dd49b4a77b8e8392a1ba778aec8267273928c
Joel Messerli <hi.github@peg.nu>
2017-05-28 00:21:25 +0200

    Fix compile error by importing isTruthy

--- frontend/src/app/core/services/ui/responsive-helper.service.ts
+++ frontend/src/app/core/services/ui/responsive-helper.service.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {MATERIALIZE_MIN_WIDTH_LARGE, MATERIALIZE_MIN_WIDTH_MEDIUM, MOBILE_DIALOG} from '../../util/const';
-import {MdDialogConfig} from '@angular/material';
-import {Orientation} from './orientation';
-import {isNotNull} from '../../util/helper';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MATERIALIZE_MIN_WIDTH_LARGE, MATERIALIZE_MIN_WIDTH_MEDIUM, MOBILE_DIALOG } from '../../util/const';
+import { MdDialogConfig } from '@angular/material';
+import { Orientation } from './orientation';
+import { isTruthy } from '../../util/helper';
 
 @Injectable()
 export class ResponsiveHelperService {
@@ -23,7 +23,6 @@ export class ResponsiveHelperService {
             .distinctUntilChanged()
             .debounceTime(200)
             .map(() => {
-                // That's weird......but in another way the compiler won't compile ¯\_(ツ)_/¯
                 let orientation = isTruthy(window.screen) ? (window.screen as any).orientation.angle : window.orientation;
                 if (Math.abs(orientation) === Orientation.LANDSCAPE) {
                     return Orientation.LANDSCAPE
