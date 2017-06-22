8d5c582d8a3ca32f64d28e4ac6f087e2270f91c7
M4R1KU <mario99.kunz@gmail.com>
2017-06-22 18:01:10 +0200

    added chevron to collapsible

--- frontend/src/app/oc-ui/components/oc-collapsible/oc-collapsible.ts
+++ frontend/src/app/oc-ui/components/oc-collapsible/oc-collapsible.ts
@@ -5,7 +5,10 @@ import {
     EventEmitter,
     HostBinding,
     HostListener,
+    Input,
+    OnChanges,
     Output,
+    SimpleChanges,
     ViewEncapsulation
 } from '@angular/core';
 
@@ -13,12 +16,22 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
     selector: 'oc-collapsible-header',
-    template: '<ng-content></ng-content>',
+    template: `
+        <div class="oc-collapsible-header-content">
+            <ng-content></ng-content>
+        </div>
+        <div class="oc-collapsible-toggle" *ngIf="showToggle">
+            <md-icon>expand_more</md-icon>
+        </div>
+    `,
 })
 export class OCCollapsibleHeaderComponent {
     @Output('toggle') onClick: EventEmitter<any> = new EventEmitter();
+    public showToggle: boolean;
 
-    @HostListener('click') click() {
+
+    @HostListener('click')
+    click() {
         this.onClick.emit();
     };
 
@@ -34,10 +47,10 @@ export class OCCollapsibleBodyComponent {
 @Component({
     selector: 'oc-collapsible',
     template: `
-            <ng-content></ng-content>
-            <div [@ocToggle]="opened">
-                <ng-content select="oc-collapsible-body"></ng-content>
-            </div>`,
+        <ng-content></ng-content>
+        <div [@ocToggle]="opened">
+            <ng-content select="oc-collapsible-body"></ng-content>
+        </div>`,
     styleUrls: ['./oc-collapsible.scss'],
     encapsulation: ViewEncapsulation.None,
     animations: [
@@ -48,20 +61,26 @@ export class OCCollapsibleBodyComponent {
             state('1', style({
                 height: '*'
             })),
-            transition('0 => 1', animate('400ms ease-in')),
-            transition('1 => 0', animate('400ms ease-out'))
+            transition('0 => 1', animate('225ms cubic-bezier(0.4,0.0,0.2,1)')),
+            transition('1 => 0', animate('225ms cubic-bezier(0.4,0.0,0.2,1)'))
         ])
     ]
 })
-export class OCCollapsibleComponent implements AfterContentInit {
+export class OCCollapsibleComponent implements AfterContentInit, OnChanges {
     @ContentChild(OCCollapsibleHeaderComponent) public header: OCCollapsibleHeaderComponent;
     @ContentChild(OCCollapsibleBodyComponent) public body: OCCollapsibleBodyComponent;
+    @Input() showToggle: boolean = true;
+
+    @HostBinding('class.active') public opened: boolean = false;
 
     ngAfterContentInit(): void {
         this.header.onClick.subscribe(() => this.toggle());
+        this.ngOnChanges(null);
     }
 
-    @HostBinding('class.active') public opened: boolean = false;
+    ngOnChanges(changes: SimpleChanges): void {
+        this.header.showToggle = this.showToggle;
+    }
 
     toggle() {
         this.opened = !this.opened;
