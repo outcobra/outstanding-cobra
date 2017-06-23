2b3d3b0a4560e2ff72ff935b006a26f38365348d
Mario Kunz BIT <mario.kunz@bit.admin.ch>
2017-06-23 16:43:13 +0200

    provide mark

--- frontend/src/app/mark/mark-value/mark-value.component.spec.ts
+++ frontend/src/app/mark/mark-value/mark-value.component.spec.ts
@@ -2,6 +2,7 @@ import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 import {MarkValueComponent} from './mark-value.component';
 import {TestModule} from '../../core/mock/test.module';
+import {MockMarkService} from '../../core/mock/mark/mock-mark.service';
 
 describe('MarkValueComponent', () => {
     let component: MarkValueComponent;
@@ -22,6 +23,7 @@ describe('MarkValueComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(MarkValueComponent);
         component = fixture.componentInstance;
+        component.mark = MockMarkService.SUBJET_MARK_GROUP_1.markValues[0];
         fixture.detectChanges();
     });
 
