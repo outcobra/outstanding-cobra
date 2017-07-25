d35da34827ceaaeaabb9c12eba844da87c743fe3
Mario Kunz BIT <mario99.kunz@gmail.com>
2017-07-25 16:51:50 +0200

    update can now be activated mostly style changes

--- frontend/src/app/mark/mark-weight-updater/mark-weight-updater.component.ts
+++ frontend/src/app/mark/mark-weight-updater/mark-weight-updater.component.ts
@@ -6,11 +6,17 @@ import {FormBuilder, FormGroup} from '@angular/forms';
     selector: 'mark-weight-updater',
     templateUrl: './mark-weight-updater.component.html',
     styleUrls: ['./mark-weight-updater.component.scss'],
-    encapsulation: ViewEncapsulation.None
+    encapsulation: ViewEncapsulation.None,
+    host: {
+        '[class.active]': 'active',
+        '(document: click)': 'onDocumentClick($event)',
+        '(click)': 'onHostClick()'
+    }
 })
 export class MarkWeightUpdaterComponent implements OnInit {
     @Input() markGroup: MarkGroupDto;
     private _weightUpdaterForm: FormGroup;
+    private _active: boolean = false;
 
     constructor(private _formBuilder: FormBuilder) {
     }
@@ -21,8 +27,19 @@ export class MarkWeightUpdaterComponent implements OnInit {
         });
     }
 
+    public onHostClick() {
+        this._active = true;
+    }
+
+    public onDocumentClick() {
+
+    }
+
     get weightUpdaterForm(): FormGroup {
         return this._weightUpdaterForm;
     }
 
+    get active(): boolean {
+        return this._active;
+    }
 }
