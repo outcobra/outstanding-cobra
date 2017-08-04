16a46750a889d10036541f21345c1b509f7276f0
Florian Bürgi <fb.buergi@gmx.ch>
2017-08-04 09:19:10 +0200

    fixed migration scripts

--- backend/src/main/resources/db/migration/V1_1_2__update-exam.sql
+++ backend/src/main/resources/db/migration/V1_1_2__update-exam.sql
@@ -1,7 +1,16 @@
 ALTER TABLE exam
   ADD COLUMN description VARCHAR(255) NULL;
 
+UPDATE exam
+SET description = ''
+WHERE description IS NULL;
+
+ALTER TABLE exam
+  MODIFY description VARCHAR(255) NOT NULL;
+
 ALTER TABLE exam_task
-  DROP FOREIGN KEY FK1yg6686i4k5pp0bhjqcn3gwwh,
+  DROP FOREIGN KEY FK1yg6686i4k5pp0bhjqcn3gwwh;
+
+ALTER TABLE exam_task
   ADD CONSTRAINT examTask_Exam FOREIGN KEY (exam_id) REFERENCES exam (id)
   ON DELETE CASCADE;
