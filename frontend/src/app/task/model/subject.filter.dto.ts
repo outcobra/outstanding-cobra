ba10d056c31d31f9f72fad7e3be3d715cc0ac670
Mario Kunz <mario99.kunz@gmail.com>
2017-08-06 22:22:04 +0200

    added taskfilter optgroups

--- frontend/src/app/task/model/subject.filter.dto.ts
+++ frontend/src/app/task/model/subject.filter.dto.ts
@@ -3,6 +3,6 @@ import {SchoolClassDto, SubjectDto} from '../../manage/model/manage.dto';
 export interface SubjectFilterDto {
     schoolClassSubjects: [{
         schoolClass: SchoolClassDto,
-        subject: Array<SubjectDto>
+        subjects: Array<SubjectDto>
     }]
 }
