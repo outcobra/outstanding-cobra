e8ca3b981d52f85abada37292ddac6d735fa6834
Mario Kunz <mario99.kunz@gmail.com>
2017-08-09 22:02:35 +0200

    use the material2 overlay api for the search field

--- frontend/src/app/oc-ui/directives/oc-auto-focus.directive.ts
+++ frontend/src/app/oc-ui/directives/oc-auto-focus.directive.ts
@@ -1,14 +1,19 @@
-import {AfterViewInit, Directive, ElementRef, Input} from '@angular/core';
+import {AfterViewInit, Directive, ElementRef, Input, OnInit} from '@angular/core';
 import {isTrue, isTruthy} from '../../core/util/helper';
 
 @Directive({
     selector: '[ocAutoFocus]'
 })
-export class OCAutoFocusDirective implements AfterViewInit {
-    @Input() public ocAutoFocus = true;
-    @Input() public ocAutoSelect = false;
+export class OCAutoFocusDirective implements OnInit, AfterViewInit {
+    @Input() public ocAutoFocus;
+    @Input() public ocAutoSelect;
 
     constructor(private el: ElementRef) {
+    }
+
+    ngOnInit() {
+        this.ocAutoFocus = this.ocAutoFocus || true;
+        this.ocAutoSelect = this.ocAutoSelect || false;
     }
 
     ngAfterViewInit() {
