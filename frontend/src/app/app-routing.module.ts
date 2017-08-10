c4abdb27df7bab612a7cb121e8c16f7b0f51ce93
M4R1KU <mario99.kunz@gmail.com>
2017-08-10 12:12:22 +0200

    address review

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {FallbackComponent} from './main/fallback/fallback.component';
-import {AuthGuard} from './core/services/auth/auth-guard.service';
 
 @NgModule({
     imports: [
         RouterModule.forRoot([{
             path: '**',
-            component: FallbackComponent,
-            canActivate: [AuthGuard],
+            component: FallbackComponent
         }])
     ],
     exports: [
