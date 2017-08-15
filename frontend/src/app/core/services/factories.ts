ce66dd5b22b1b0e58155c1bb9b81339a50ada229
Mario Kunz <mario99.kunz@gmail.com>
2017-08-15 23:47:11 +0200

    updated angular dependencies

--- frontend/src/app/core/services/factories.ts
+++ frontend/src/app/core/services/factories.ts
@@ -1,11 +1,11 @@
 import {ConfigService} from '../config/config.service';
-import {Http} from '@angular/http';
 import {TranslateService} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {LOCATION_INITIALIZED} from '@angular/common';
 import {Injector} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
-export function translateFactory(http: Http) {
+export function translateFactory(http: HttpClient) {
     return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
 }
 
