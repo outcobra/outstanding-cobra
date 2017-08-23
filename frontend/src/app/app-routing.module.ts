147b2082f3adc91cd38806350a72f4b1393109f0
M4R1KU <mario99.kunz@gmail.com>
2017-08-23 07:25:49 +0200

    add beta span after logo i18n-markdown can now be fed with route data

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {FallbackComponent} from './main/fallback/fallback.component';
+import {I18nMarkdownComponent} from './shared/components/i18n-markdown/i18n-markdown.component';
 
 @NgModule({
     imports: [
-        RouterModule.forRoot([{
-            path: '**',
-            component: FallbackComponent
-        }])
+        RouterModule.forRoot([
+            {
+                path: 'about',
+                component: I18nMarkdownComponent,
+                data: {
+                    path: '/assets/docs/test'
+                }
+            },
+            {
+                path: '**',
+                component: FallbackComponent
+            }
+        ])
     ],
     exports: [
         RouterModule
--- frontend/src/app/shared/components/i18n-markdown/i18n-markdown.component.ts
+++ frontend/src/app/shared/components/i18n-markdown/i18n-markdown.component.ts
@@ -1,24 +1,40 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
-import {isNotEmpty} from '../../../core/util/helper';
+import {isNotEmpty, isTruthy} from '../../../core/util/helper';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
     selector: 'i18n-markdown',
-    template: `<markdown [path]="i18nPath"></markdown>`,
+    template: `
+        <markdown [path]="i18nPath"></markdown>`,
     encapsulation: ViewEncapsulation.Native
 })
-export class I18nMarkdownComponent implements OnInit {
+export class I18nMarkdownComponent implements OnInit, OnChanges {
     @Input() path: string;
 
     private _i18nPath: string;
 
-    constructor(private _translateService: TranslateService) {
+    constructor(private _route: ActivatedRoute,
+                private _translateService: TranslateService) {
     }
 
     ngOnInit(): void {
         if (isNotEmpty(this.path)) {
             this._setPathForCurrentLang();
-            this._translateService.onLangChange.subscribe(() => this._setPathForCurrentLang())
+        }
+
+        this._route.data
+            .map(data => data.path)
+            .filter(isNotEmpty)
+            .do(path => this.path = path)
+            .subscribe(() => this._setPathForCurrentLang());
+
+        this._translateService.onLangChange.subscribe(() => this._setPathForCurrentLang())
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        if (isTruthy(changes.path)) {
+            this._setPathForCurrentLang();
         }
     }
 
