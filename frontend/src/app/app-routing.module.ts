3715cfde199d0d13850e3fe14049c5fb68ade7bf
Mario Kunz <mario99.kunz@gmail.com>
2017-09-02 18:33:02 +0200

    add about add bottom nav

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,20 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {FallbackComponent} from './main/fallback/fallback.component';
-import {I18nMarkdownComponent} from './shared/components/i18n-markdown/i18n-markdown.component';
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
-            {
-                path: 'features',
-                component: I18nMarkdownComponent,
-                data: {
-                    path: '/assets/docs/features',
-                    displayMode: 'card',
-                    wrapperClasses: ['container', 'middle-container', 'features-text']
-                }
-            },
             {
                 path: '**',
                 component: FallbackComponent
