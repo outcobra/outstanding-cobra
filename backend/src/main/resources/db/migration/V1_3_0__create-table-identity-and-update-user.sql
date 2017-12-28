88992768455412ba5b612c9555790e10e1ab6464
Joel Messerli <hi.github@peg.nu>
2017-12-28 20:26:34 +0100

    Untested version of db migration because my docker is acting up

--- backend/src/main/resources/db/migration/V1_3_0__create-table-identity-and-update-user.sql
+++ backend/src/main/resources/db/migration/V1_3_0__create-table-identity-and-update-user.sql
@@ -15,7 +15,31 @@ CREATE UNIQUE INDEX idx_identity_identifier
   ON identity (identifier);
 
 ALTER TABLE USER
-ADD COLUMN MAIL VARCHAR(100) NOT NULL;
+  ADD COLUMN MAIL VARCHAR(100);
+
+CREATE PROCEDURE migrate_auth0_google()
+  BEGIN
+    DECLARE auth0_cur CURSOR FOR
+      SELECT
+        id,
+        auth0id
+      FROM user
+      WHERE auth0id LIKE 'google-oauth2|%'; -- length: 14
+
+    DECLARE cur_user BIGINT(20);
+    DECLARE auth0id VARCHAR(255);
+
+    LOOP
+      FETCH auth0_cur
+      INTO cur_user, auth0id;
+
+      INSERT INTO identity (user, identity_type, identifier)
+      VALUES (cur_user, 'google-auth', substring(auth0id, 14));
+    END LOOP;
+  END;
+
+CALL migrate_auth0_google();
+DROP PROCEDURE migrate_auth0_google;
 
 ALTER TABLE USER
-DROP COLUMN AUTH0ID;
+  DROP COLUMN AUTH0ID;
