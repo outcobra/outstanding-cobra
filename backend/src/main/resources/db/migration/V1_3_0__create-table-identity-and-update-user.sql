dc29d05f18c7b262b87159457b626960e8e0acf6
Florian Bürgi <fb.buergi@gmx.ch>
2017-12-30 13:30:37 +0100

    renamed column user to user_id in migration

--- backend/src/main/resources/db/migration/V1_3_0__create-table-identity-and-update-user.sql
+++ backend/src/main/resources/db/migration/V1_3_0__create-table-identity-and-update-user.sql
@@ -1,11 +1,11 @@
 CREATE TABLE identity (
   id            BIGINT(20)   NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user          BIGINT(20)   NOT NULL,
+  user_id       BIGINT(20)   NOT NULL,
   identity_type VARCHAR(255) NOT NULL,
   identifier    VARCHAR(255) NOT NULL,
   secret        VARCHAR(255),
 
-  CONSTRAINT fk_identity_user FOREIGN KEY (user) REFERENCES user (id)
+  CONSTRAINT fk_identity_user FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
 CREATE UNIQUE INDEX idx_identity_id
