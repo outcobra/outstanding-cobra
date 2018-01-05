0ebf0bc07363fe5b502fdca3166e98be8e6e98da
Mario Kunz <mario.kunz@bit.admin.ch>
2018-01-05 13:17:52 +0100

    add go to app button

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,16 +1,13 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../core/interfaces/auth.service';
+import {DefaultAuthService} from '../core/services/auth/auth.service';
 
 @Component({
     selector: 'main',
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
-
-    constructor() {
+export class MainComponent {
+    constructor(public authService: DefaultAuthService) {
     }
-
-    ngOnInit() {
-    }
-
 }
