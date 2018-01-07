87b7cb8537ec17a92cbf85e4a0d8c332c4604d4b
Florian Bürgi <fb.buergi@gmx.ch>
2018-01-07 17:14:58 +0100

    use directive to highlight insufficient marks

--- frontend/src/app/mark/directive/oc-mark-highlighter-directive.ts
+++ frontend/src/app/mark/directive/oc-mark-highlighter-directive.ts
@@ -3,16 +3,16 @@ import {Directive, ElementRef, Input, OnInit, Renderer2} from '@angular/core';
 @Directive({
     selector: '[ocMarkHighlighter]'
 })
-export class OcMarkHighlighterDirective implements OnInit{
-    @Input('ocMarkHighlighter') mark: number
+export class OcMarkHighlighterDirective implements OnInit {
+    @Input('ocMarkHighlighter') mark: number;
 
     constructor(private _elementRef: ElementRef,
                 private _renderer: Renderer2) {
     }
 
-    ngOnInit(){
-        if(this.mark<4.0) {
-            this._renderer.addClass(this._elementRef.nativeElement, 'insufficient')
+    ngOnInit() {
+        if (this.mark < 4.0) {
+            this._renderer.addClass(this._elementRef.nativeElement, 'insufficient');
         }
     }
 }
