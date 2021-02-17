cc0e23b31aecba0cff109a7d6379b535e1ffdf4d
Mario Kunz <mario99.kunz@gmail.com>
2021-02-17 10:13:51 +0100

    update to angular material 9

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,6 @@ import {enableProdMode} from '@angular/core';
 import {environment} from './environments/environment';
 import {AppModule} from './app/';
 import './rx-operators'
-import 'hammerjs';
 import {registerLocaleData} from '@angular/common';
 import localeDeCh from '@angular/common/locales/de-CH';
 
